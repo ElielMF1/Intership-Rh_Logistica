0e16417c309530795fb460d10d2b1ccdcf0f2945
ElielMF1 <108632422+ElielMF1@users.noreply.github.com>
2022-07-03 16:03:42 -0500

    Add files via upload

---
+++ Tacubaya/jsinversa.js
@@ -0,0 +1,137 @@
+
+// ----------------------------------------carga
+{
+
+let caja= document.getElementById("dev")
+
+caja.addEventListener("click",()=>{
+
+let mensaje_= document.getElementById('caja-dev')
+mensaje_.classList.add("caja_visible")
+
+})
+
+}
+
+{
+let cerrar= document.getElementById('Cerrar_')
+
+cerrar.addEventListener("click",()=>{
+
+let ventana= document.getElementById('caja-dev')
+
+ventana.classList.remove("caja_visible")
+
+})
+
+}
+// ----------------------------------------revision
+{
+
+let caja= document.getElementById("evaluacion")
+
+caja.addEventListener("click",()=>{
+
+let mensaje_= document.getElementById('caja-evaluacion')
+mensaje_.classList.add("caja_visible")
+
+})
+
+}
+
+{
+let cerrar= document.getElementById('Cerrar_1')
+
+cerrar.addEventListener("click",()=>{
+
+let ventana= document.getElementById('caja-evaluacion')
+
+ventana.classList.remove("caja_visible")
+
+})
+
+}
+
+// ----------------------------------------llamada
+{
+
+let caja= document.getElementById("maderas")
+
+caja.addEventListener("click",()=>{
+
+let mensaje_= document.getElementById('caja-maderas')
+mensaje_.classList.add("caja_visible")
+
+})
+
+}
+
+{
+let cerrar= document.getElementById('Cerrar_2')
+
+cerrar.addEventListener("click",()=>{
+
+let ventana= document.getElementById('caja-maderas')
+
+ventana.classList.remove("caja_visible")
+
+})
+
+}
+
+// ----------------------------------------entrega
+{
+
+let caja= document.getElementById("blanca")
+
+caja.addEventListener("click",()=>{
+
+let mensaje_= document.getElementById('caja-blanca')
+mensaje_.classList.add("caja_visible")
+
+})
+
+}
+
+{
+let cerrar= document.getElementById('Cerrar_3')
+
+cerrar.addEventListener("click",()=>{
+
+let ventana= document.getElementById('caja-blanca')
+
+ventana.classList.remove("caja_visible")
+
+})
+
+}
+
+// ----------------------------------------siguiente
+{
+
+let caja= document.getElementById("tapizado")
+
+caja.addEventListener("click",()=>{
+
+let mensaje_= document.getElementById('caja-tapizado')
+mensaje_.classList.add("caja_visible")
+
+})
+
+}
+
+{
+let cerrar= document.getElementById('Cerrar_4')
+
+cerrar.addEventListener("click",()=>{
+
+let ventana= document.getElementById('caja-tapizado')
+
+ventana.classList.remove("caja_visible")
+
+})
+
+}
+
+
+
