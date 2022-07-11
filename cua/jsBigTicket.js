188d8a5dc073bf59405c8b6df799c670e7adfce6
ElielMF1 <108632422+ElielMF1@users.noreply.github.com>
2022-07-11 00:52:35 -0500

    Add files via upload

---
+++ cua/jsBigTicket.js
@@ -0,0 +1,163 @@
+
+// ----------------------------------------carga
+{
+
+let caja= document.getElementById("carga")
+
+caja.addEventListener("click",()=>{
+
+let mensaje_= document.getElementById('caja-carga')
+mensaje_.classList.add("caja_visible")
+
+})
+
+}
+
+{
+let cerrar= document.getElementById('Cerrar_')
+
+cerrar.addEventListener("click",()=>{
+
+let ventana= document.getElementById('caja-carga')
+
+ventana.classList.remove("caja_visible")
+
+})
+
+}
+// ----------------------------------------revision
+{
+
+let caja= document.getElementById("revision")
+
+caja.addEventListener("click",()=>{
+
+let mensaje_= document.getElementById('caja-revision')
+mensaje_.classList.add("caja_visible")
+
+})
+
+}
+
+{
+let cerrar= document.getElementById('Cerrar_1')
+
+cerrar.addEventListener("click",()=>{
+
+let ventana= document.getElementById('caja-revision')
+
+ventana.classList.remove("caja_visible")
+
+})
+
+}
+
+// ----------------------------------------llamada
+{
+
+let caja= document.getElementById("call")
+
+caja.addEventListener("click",()=>{
+
+let mensaje_= document.getElementById('caja-call')
+mensaje_.classList.add("caja_visible")
+
+})
+
+}
+
+{
+let cerrar= document.getElementById('Cerrar_2')
+
+cerrar.addEventListener("click",()=>{
+
+let ventana= document.getElementById('caja-call')
+
+ventana.classList.remove("caja_visible")
+
+})
+
+}
+
+// ----------------------------------------entrega
+{
+
+let caja= document.getElementById("entrega")
+
+caja.addEventListener("click",()=>{
+
+let mensaje_= document.getElementById('caja-entrega')
+mensaje_.classList.add("caja_visible")
+
+})
+
+}
+
+{
+let cerrar= document.getElementById('Cerrar_3')
+
+cerrar.addEventListener("click",()=>{
+
+let ventana= document.getElementById('caja-entrega')
+
+ventana.classList.remove("caja_visible")
+
+})
+
+}
+
+// ----------------------------------------siguiente
+{
+
+let caja= document.getElementById("siguiente")
+
+caja.addEventListener("click",()=>{
+
+let mensaje_= document.getElementById('caja-siguiente')
+mensaje_.classList.add("caja_visible")
+
+})
+
+}
+
+{
+let cerrar= document.getElementById('Cerrar_4')
+
+cerrar.addEventListener("click",()=>{
+
+let ventana= document.getElementById('caja-siguiente')
+
+ventana.classList.remove("caja_visible")
+
+})
+
+}
+
+// ----------------------------------------manifiesto
+{
+
+let caja= document.getElementById("manif")
+
+caja.addEventListener("click",()=>{
+
+let mensaje_= document.getElementById('caja-manif')
+mensaje_.classList.add("caja_visible")
+
+})
+
+}
+
+{
+let cerrar= document.getElementById('Cerrar_5')
+
+cerrar.addEventListener("click",()=>{
+
+let ventana= document.getElementById('caja-manif')
+
+ventana.classList.remove("caja_visible")
+
+})
+
+}
+
+
