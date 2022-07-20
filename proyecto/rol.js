2723783b40368b9b30e218cf24d214836741d21b
ElielMF1 <108632422+ElielMF1@users.noreply.github.com>
2022-07-19 19:44:45 -0500

    Add files via upload

---
+++ proyecto/rol.js
@@ -0,0 +1,378 @@
+
+
+
+// // ----------------------------------------Compras
+// {
+
+// let caja= document.getElementById("compras")
+
+// caja.addEventListener("click",()=>{
+
+// let mensaje_= document.getElementById('caja-compras')
+// mensaje_.classList.add("caja_visible")
+
+// })
+
+// }
+
+// {
+// let cerrar= document.getElementById('Cerrar_')
+
+// cerrar.addEventListener("click",()=>{
+
+// let ventana= document.getElementById('caja-compras')
+
+// ventana.classList.remove("caja_visible")
+
+// })
+
+// }
+// // ----------------------------------------citas
+// {
+
+// let caja= document.getElementById("citas")
+
+// caja.addEventListener("click",()=>{
+
+// let mensaje_= document.getElementById('caja-citas')
+// mensaje_.classList.add("caja_visible")
+
+// })
+
+// }
+
+// {
+// let cerrar= document.getElementById('Cerrar_1')
+
+// cerrar.addEventListener("click",()=>{
+
+// let ventana= document.getElementById('caja-citas')
+
+// ventana.classList.remove("caja_visible")
+
+// })
+
+// }
+
+// // -----------------------------------recepcion
+// {
+
+// let caja= document.getElementById("torre")
+
+// caja.addEventListener("click",()=>{
+
+// let mensaje_= document.getElementById('caja-torre')
+// mensaje_.classList.add("caja_visible")
+
+// })
+
+// }
+
+// {
+// let cerrar= document.getElementById('Cerrar_2')
+
+// cerrar.addEventListener("click",()=>{
+
+// let ventana= document.getElementById('caja-torre')
+
+// ventana.classList.remove("caja_visible")
+
+// })
+
+// }
+
+// // ----------------------------------------recepcion
+// {
+
+// let caja= document.getElementById("recepcion")
+
+// caja.addEventListener("click",()=>{
+
+// let mensaje_= document.getElementById('caja-recepcion')
+// mensaje_.classList.add("caja_visible")
+
+// })
+
+// }
+
+// {
+// let cerrar= document.getElementById('Cerrar_3')
+
+// cerrar.addEventListener("click",()=>{
+
+// let ventana= document.getElementById('caja-recepcion')
+
+// ventana.classList.remove("caja_visible")
+
+// })
+
+// }
+
+// // ----------------------------------------descarga
+// {
+
+// let caja= document.getElementById("descarga")
+
+// caja.addEventListener("click",()=>{
+
+// let mensaje_= document.getElementById('caja-descarga')
+// mensaje_.classList.add("caja_visible")
+
+// })
+
+// }
+
+// {
+// let cerrar= document.getElementById('Cerrar_4')
+
+// cerrar.addEventListener("click",()=>{
+
+// let ventana= document.getElementById('caja-descarga')
+
+// ventana.classList.remove("caja_visible")
+
+// })
+
+// }
+
+// // ----------------------------------------decalidad
+// {
+
+// let caja= document.getElementById("calidad")
+
+// caja.addEventListener("click",()=>{
+
+// let mensaje_= document.getElementById('caja-calidad')
+// mensaje_.classList.add("caja_visible")
+
+// })
+
+// }
+
+// {
+// let cerrar= document.getElementById('Cerrar_5')
+
+// cerrar.addEventListener("click",()=>{
+
+// let ventana= document.getElementById('caja-calidad')
+
+// ventana.classList.remove("caja_visible")
+
+// })
+
+// }
+
+// // ----------------------------------------pulmones
+// {
+
+// let caja= document.getElementById("pulmones")
+
+// caja.addEventListener("click",()=>{
+
+// let mensaje_= document.getElementById('caja-pulmones')
+// mensaje_.classList.add("caja_visible")
+
+// })
+
+// }
+
+// {
+// let cerrar= document.getElementById('Cerrar_6')
+
+// cerrar.addEventListener("click",()=>{
+
+// let ventana= document.getElementById('caja-pulmones')
+
+// ventana.classList.remove("caja_visible")
+
+// })
+
+// }
+// // ----------------------------------------bloque
+// {
+
+// let caja= document.getElementById("bloque")
+
+// caja.addEventListener("click",()=>{
+
+// let mensaje_= document.getElementById('caja-bloque')
+// mensaje_.classList.add("caja_visible")
+
+// })
+
+// }
+
+// {
+// let cerrar= document.getElementById('Cerrar_7')
+
+// cerrar.addEventListener("click",()=>{
+
+// let ventana= document.getElementById('caja-bloque')
+
+// ventana.classList.remove("caja_visible")
+
+// })
+
+// }
+
+// // ----------------------------------------cantiliver
+// {
+
+// let caja= document.getElementById("cantiliver")
+
+// caja.addEventListener("click",()=>{
+
+// let mensaje_= document.getElementById('caja-cantiliver')
+// mensaje_.classList.add("caja_visible")
+
+// })
+
+// }
+
+// {
+// let cerrar= document.getElementById('Cerrar_8')
+
+// cerrar.addEventListener("click",()=>{
+
+// let ventana= document.getElementById('caja-cantiliver')
+
+// ventana.classList.remove("caja_visible")
+
+// })
+
+// }
+
+// // ----------------------------------------inventarios
+// {
+
+// let caja= document.getElementById("inventarios")
+
+// caja.addEventListener("click",()=>{
+
+// let mensaje_= document.getElementById('caja-inventarios')
+// mensaje_.classList.add("caja_visible")
+
+// })
+
+// }
+
+// {
+// let cerrar= document.getElementById('Cerrar_9')
+
+// cerrar.addEventListener("click",()=>{
+
+// let ventana= document.getElementById('caja-inventarios')
+
+// ventana.classList.remove("caja_visible")
+
+// })
+
+// }
+
+// // ----------------------------------------planeacion
+// {
+
+// let caja= document.getElementById("planeacion")
+
+// caja.addEventListener("click",()=>{
+
+// let mensaje_= document.getElementById('caja-planeacion')
+// mensaje_.classList.add("caja_visible")
+
+// })
+
+// }
+
+// {
+// let cerrar= document.getElementById('Cerrar_10')
+
+// cerrar.addEventListener("click",()=>{
+
+// let ventana= document.getElementById('caja-planeacion')
+
+// ventana.classList.remove("caja_visible")
+
+// })
+
+// }
+
+// // ----------------------------------------surtido
+// {
+
+// let caja= document.getElementById("surtido")
+
+// caja.addEventListener("click",()=>{
+
+// let mensaje_= document.getElementById('caja-surtido')
+// mensaje_.classList.add("caja_visible")
+
+// })
+
+// }
+
+// {
+// let cerrar= document.getElementById('Cerrar_11')
+
+// cerrar.addEventListener("click",()=>{
+
+// let ventana= document.getElementById('caja-surtido')
+
+// ventana.classList.remove("caja_visible")
+
+// })
+
+// }
+
+// // ----------------------------------------envios
+// {
+
+// let caja= document.getElementById("envios")
+
+// caja.addEventListener("click",()=>{
+
+// let mensaje_= document.getElementById('caja-envios')
+// mensaje_.classList.add("caja_visible")
+
+// })
+
+// }
+
+// {
+// let cerrar= document.getElementById('Cerrar_12')
+
+// cerrar.addEventListener("click",()=>{
+
+// let ventana= document.getElementById('caja-envios')
+
+// ventana.classList.remove("caja_visible")
+
+// })
+
+// }
+
+// // ----------------------------------------Inversa
+// {
+
+// let caja= document.getElementById("inversa_12")
+
+// caja.addEventListener("click",()=>{
+
+// let mensaje_= document.getElementById('caja-inversa_12')
+// mensaje_.classList.add("caja_visible")
+
+// })
+
+// }
+
+// {
+// let cerrar= document.getElementById('Cerrar_13')
+
+// cerrar.addEventListener("click",()=>{
+
+// let ventana= document.getElementById('caja-inversa_12')
+
+// ventana.classList.remove("caja_visible")
+
+// })
+
+// }			
